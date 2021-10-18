da6be681227c46d3ecb437d86a35b0163d3c5c4d
ExceedWu <91442891+ExceedWu@users.noreply.github.com>
2021-10-18 11:26:40 +0800

    Add files via upload

---
+++ wow.java
@@ -0,0 +1,23 @@
+
+package nml;
+
+class hw2{
+	int width;
+	int height;
+	
+	hw2(int w , int h){
+		this.width = w;
+		this.height = h;
+	}
+}
+
+public class wow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hw2 a = new hw2(10,8);
+		System.out.println("width:"+a.width);
+		System.out.println("height:"+a.height);
+	}
+
+}
