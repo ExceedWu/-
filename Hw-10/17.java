48310bc1d80cd346ba8fc2c449e4307f76b45b0c
ExceedWu <91442891+ExceedWu@users.noreply.github.com>
2021-11-15 03:00:27 +0800

    Create 17.java

---
+++ Hw-10/17.java
@@ -0,0 +1,33 @@
+class test {
+    protected String owner;
+    protected String id;
+    public test(String own,String s) {
+        owner=own;
+        id=s;
+    }
+    final void show() {
+        System.out.println("車主姓名:"+owner);
+        System.out.println("車牌號碼:"+id);
+    }
+}
+class test1 extends test {
+    public String color;
+    public test1(String own,String s,String col) {
+        super(own,s);
+        color=col;
+    }
+    public void show()
+    {
+        System.out.println("車主姓名:"+owner);
+        System.out.println("車牌號碼:"+id);
+        System.out.println("車身顏色:"+color);
+    }
+}
+
+public class Nml {
+    public static void main(String args[]) {
+        test1 x=new test1("Riaan","A1-2345","Black");
+        x.show();
+        System.out.println("車身顏色:"+x.color);
+    }
+}
