3ac5876af3ac090d36a9f0ed93553b82108be022
ExceedWu <91442891+ExceedWu@users.noreply.github.com>
2022-01-10 03:32:48 +0800

    Create 15.java

---
+++ Hw-12/15.java
@@ -0,0 +1,8 @@
+public class Nml {
+    public static void main(String args[]) {
+        String str;
+        str = Float.toString(123.45f);
+        System.out.println(str);
+        System.out.println(str.length());
+    }
+}
