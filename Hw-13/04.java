45a378b83992c67bc467540f887f40ef752b0f93
ExceedWu <91442891+ExceedWu@users.noreply.github.com>
2022-01-10 02:43:51 +0800

    Create 04.java

---
+++ Hw-13/04.java
@@ -0,0 +1,9 @@
+public class Nml {
+    public static void main(String args[]) {
+        int num=12;
+        int den[]={12,0,3,0,0,4};
+        for(int i=0;i<10;i++){
+            System.out.println("ans = "+num/den[i]);
+        }
+    }
+}
