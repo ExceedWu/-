46bebe5b9912941fe06d7cae71beb0d2501f0b76
ExceedWu <91442891+ExceedWu@users.noreply.github.com>
2022-01-10 03:31:51 +0800

    Create 14.java

---
+++ Hw-12/14.java
@@ -0,0 +1,7 @@
+public class Nml {
+    public static void main(String args[]) {
+        String str="Habit is second nature.";
+        System.out.println(str);
+        System.out.println(str.substring(16,22));
+    }
+}
